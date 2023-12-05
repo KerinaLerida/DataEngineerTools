b2c546b2005863bc86780d853bd1d4d11ac44338
“Keren <kerencouton@gmail.com>
2023-12-05 15:36:35 +0100

    1Introduction finished

--- 2Scrapy/citations_churchill_spider1.py
+++ 2Scrapy/citations_churchill_spider1.py
@@ -5,6 +5,13 @@ class ChurchillQuotesSpider(scrapy.Spider):
     start_urls = ["http://evene.lefigaro.fr/citations/winston-churchill",]
 
     def parse(self, response):
+        # Trouver l'auteur sur la page
+        author = response.css('a[href="/celebre/biographie/winston-churchill-675.php"]::text').extract_first()
+
         for cit in response.xpath('//div[@class="figsco__quote__text"]'):
-            text_value = cit.xpath('a/text()').extract_first()
-            yield { 'text' : text_value }
+            text_value = cit.xpath('a//text()').extract_first()
+
+            # Supprimer les caractères “ et ” avec la méthode replace
+            cleaned_text = text_value.replace('“', '').replace('”', '')
+
+            yield {'author': author, 'text': cleaned_text}
